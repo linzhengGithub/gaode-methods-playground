9c0bac6fa0628ed6f8adca89d6854c0c1aefa158
linzheng <alidlg9045@126.com>
2024-01-04 01:22:08 +0800

    add antfu/eslint-config

--- src/test/test.spec.ts
+++ src/test/test.spec.ts
@@ -1,9 +1,9 @@
-import { it, expect } from 'vitest'
+import { expect, it } from 'vitest'
 
 function sum(a: number, b: number) {
   return a + b
 }
 
 it('happy path', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(3)
 })

ce236f217f1f2552f6c3233a57b7ae83028281a2
linzheng <alidlg9045@126.com>
2024-02-25 15:14:31 +0800

    rectangle:  happy path

---
+++ src/composables/test/useRectangle.spec.ts
@@ -0,0 +1,14 @@
+import { describe, expect, it } from 'vitest'
+import { useRectangle } from '../useRectangle'
+
+describe('rectangle test group', () => {
+  it('should be a rectangle', () => {
+    const { createRectangle, rectangleList } = useRectangle()
+    const southWest: [number, number] = [116.356449, 39.859008] // 西南角的经纬度坐标
+    const northEast: [number, number] = [116.417901, 39.893797] // 东北角的经纬度坐标
+
+    createRectangle(southWest, northEast)
+
+    expect(rectangleList.length).toBe(1)
+  })
+})
---
+++ src/composables/useRectangle.ts
@@ -0,0 +1,32 @@
+import { extend } from '@/utils'
+
+export function useRectangle(map?: AMap.Map) {
+  const rectangleList: AMap.Rectangle[] = []
+
+  function createRectangle(southWest: [number, number], northEast: [number, number], opts?: AMap.RectangleOptions) {
+    const rectangle = new AMap.Rectangle({
+      bounds: new AMap.Bounds(southWest, northEast),
+      ...extend({
+        strokeColor: 'red', // 轮廓线颜色
+        strokeWeight: 6, // 轮廓线宽度
+        strokeOpacity: 0.5, // 轮廓线透明度
+        strokeStyle: 'dashed', // 轮廓线样式，dashed 虚线，还支持 solid 实线
+        strokeDasharray: [30, 10], // 勾勒形状轮廓的虚线和间隙的样式，30代表线段长度 10代表间隙长度
+        fillColor: 'blue', // 矩形填充颜色
+        fillOpacity: 0.5, // 矩形填充透明度
+        cursor: 'pointer', // 指定鼠标悬停时的鼠标样式
+        zIndex: 50, // 矩形在地图上的层级
+      }, opts),
+    })
+
+    rectangleList.push(rectangle)
+    map?.add(rectangle)
+
+    return rectangle
+  }
+
+  return {
+    rectangleList,
+    createRectangle,
+  }
+}
